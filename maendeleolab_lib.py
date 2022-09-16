accdae0eece1a053da92fb88f832432dd47643b9
Pat@Maendeleolab <maendeleolab@gmail.com>
2022-09-16 18:52:57 -0400

    fixed paths in maendeleolab_lib.py

--- maendeleolab_lib.py
+++ maendeleolab_lib.py
@@ -6,17 +6,17 @@ from build_route import make_route_to_transit_gateway, make_route_to_nat_gateway
 from build_route import make_route_to_gateway, make_route_to_interface_id
 from build_route import destroy_route, erase_routes, erase_route_entries
 FPATH = os.environ.get('ENV_FPATH') #ENV_FPATH is in your environment variable file
-sys.path.append(FPATH+'/maendeleolab_route/vpc')
+sys.path.append(FPATH+'/maendeleolab_vpc')
 import build_vpc
-sys.path.append(FPATH+'/maendeleolab_route/subnets')
+sys.path.append(FPATH+'/maendeleolab_subnet')
 import build_subnet
-sys.path.append(FPATH+'/maendeleolab_route/transit_gateway')
-import build_transit_gateway
-sys.path.append(FPATH+'/maendeleolab_route/nat_gateway')
-import build_nat_gateway
-sys.path.append(FPATH+'/maendeleolab_route/internet_gateway')
-import build_igw
-sys.path.append(FPATH+'/maendeleolab_route/network_interfaces')
-import build_network_interfaces
+#sys.path.append(FPATH+'/maendeleolab_transitGateway')
+#import build_transit_gateway
+#sys.path.append(FPATH+'/maendeleolab_natGateway')
+#import build_nat_gateway
+#sys.path.append(FPATH+'/maendeleolab_internetGateway')
+#import build_igw
+#sys.path.append(FPATH+'/maendeleolab_networkInterface')
+#import build_network_interfaces
 
 # ----------------------- End ---------------------
